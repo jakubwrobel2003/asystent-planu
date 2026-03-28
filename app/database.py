2e0eee922b137a51087112799ac09608c599545b
jakubwrobel2003 <jakubwrobel20030404@gmail.com>
2026-03-28 19:42:22 +0100

    fix data url

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:////app/data/asystent.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data/asystent.db")
 
 engine = create_engine(
     DATABASE_URL,
